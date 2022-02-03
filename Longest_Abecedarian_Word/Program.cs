dcfabeac043fc014329b66ce6c5799bc23e4dced
dashaivasha <dasiv4nova@yandex.ru>
2022-02-03 23:49:40 +0300

    init commit

---
+++ Longest_Abecedarian_Word/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Longest_Abecedarian_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
