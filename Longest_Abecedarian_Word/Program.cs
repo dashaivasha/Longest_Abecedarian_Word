074c1daa9bcac8602754de09567a584e8b9e6736
dashaivasha <dasiv4nova@yandex.ru>
2022-02-06 17:35:47 +0300

    fix longest abecedarian word finding

--- Longest_Abecedarian_Word/Program.cs
+++ Longest_Abecedarian_Word/Program.cs
@@ -2,32 +2,42 @@
 
 namespace Longest_Abecedarian_Word
 {
+
     class Program
     {
 
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestAbecedarian(new string[] { "one", "two", "three" }));
+            Console.WriteLine(LongestAbecedarian(new string[] { "one", "aces", "hearts", "clubs" }));
+        }
+
         public static string LongestAbecedarian(string[] array)
         {
-            string Alphabet = "abcdefghijklmnopqrstuvwxyz";
-            string result = "";
-            int maxIndexOfAbecedarianWord = -1;
-            int maxLenghthOfAbecedarianWord = -1;
+            var Alphabet = "abcdefghijklmnopqrstuvwxyz";
+            var result = String.Empty;
+            var maxIndexOfAbecedarianWord = -1;
+            var maxLenghthOfAbecedarianWord = -1;
+
             for (int j = 0; j < array.Length; j++)
             {
-                string word = array[j];
-                int i;
-                int lastIndexInAlphabet = -1;
-                for (i = 0; i < word.Length; i++)
+                var word = array[j];
+                var i = 0;
+                var lastIndexInAlphabet = -1;
+
+                for (; i < word.Length; i++)
                 {
-                    int currentIndexInAlphabet = Alphabet.IndexOf(word[i]);
+                    var currentIndexInAlphabet = Alphabet.IndexOf(word[i]);
+
                     if (currentIndexInAlphabet < lastIndexInAlphabet)
                     {
                         break;
-
                     }
                     lastIndexInAlphabet = currentIndexInAlphabet;
-
                 }
-                int currentLenghthOfAbecedarianWord = i;
+
+                var currentLenghthOfAbecedarianWord = i;
+
                 if (currentLenghthOfAbecedarianWord == word.Length)
                 {
                     if (currentLenghthOfAbecedarianWord > maxLenghthOfAbecedarianWord)
@@ -35,23 +45,14 @@ namespace Longest_Abecedarian_Word
                         maxLenghthOfAbecedarianWord = currentLenghthOfAbecedarianWord;
                         maxIndexOfAbecedarianWord = j;
                     }
-
                 }
-
-
 
             }
 
-            return maxIndexOfAbecedarianWord == -1 ? "" : array[maxIndexOfAbecedarianWord];
+            result = maxIndexOfAbecedarianWord == -1 ? "" : array[maxIndexOfAbecedarianWord];
 
+            return result;
 
-        }
-
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine(LongestAbecedarian(new string[] { "one", "two", "three" }));
-            Console.WriteLine(LongestAbecedarian(new string[] { "one", "aces", "hearts", "clubs" }));
         }
     }
 }
